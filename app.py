4c34ed3a3fdb08e8f7e2c078acfbaedfb63e049d
Isaya-Ntilema <eng.isaya@gmail.com>
2023-04-19 16:27:23 +0200

    working_code

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def index():
     return Time+" Serving from "+h_name+" ("+IP_addres+")\n"
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 8080, debug = True)
+  app.run(host = '0.0.0.0', port = 80, debug = True)
   #app.run( port = 8080, debug = True)
